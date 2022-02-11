4c299055017dc97b336ac2c744a855ca38bd9ef1
J. Luke Schlosser <coolhandjls@gmail.com>
2022-02-11 15:47:35 -0500

    CoinBox method giveChange

--- capstone/src/main/java/com/techelevator/CoinBox.java
+++ capstone/src/main/java/com/techelevator/CoinBox.java
@@ -2,9 +2,9 @@ package com.techelevator;
 
 import java.math.BigDecimal;
 
-public class CoinBox {
+public class CoinBox /*extends Menu????*/ {
 
-    private int feedMoneyAmount;
+   /* private int feedMoneyAmount;
     private int moneyRemaining;
     private int totalSales;
 
@@ -20,6 +20,31 @@ public class CoinBox {
     public int getChange(){
 
         return 0;
+    }
+*/
+
+    public void giveChange(double amountRecieved){
+
+    int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+
+	while (amountRecieved >= 0.25){
+        quarters ++;
+        amountRecieved -= 0.25;
+    }
+
+	while (amountRecieved >= 0.10){
+        dimes ++;
+        amountRecieved -= 0.10;
+    }
+
+	while(amountRecieved >= 0.05){
+        nickels ++;
+        amountRecieved -= 0.05;
+    }
+
+        System.out.println("Your change is " + quarters + "quarters, " + dimes + "dimes, " + nickels + "nickels.");
     }
 
 
